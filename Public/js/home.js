b4f0eb82c59c898ca14fdf44089b546a86562300
Shehab Fekry <shehabeddin99@gmail.com>
2022-03-03 00:18:36 +0200

    new updates by shehab

--- Public/js/home.js
+++ Public/js/home.js
@@ -65,21 +65,21 @@ document.addEventListener('DOMContentLoaded', () => {
 // ------------------------------------------- Chart.js --------------------------------------------------
 
 
-const ctx = document.getElementById('myChart');
-    const myChart = new Chart(ctx, {
-        type: 'bar',
+const ctx1 = document.getElementById('pieChart');
+    const pieChart = new Chart(ctx1, {
+        type: 'pie',
         data: {
             labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
             datasets: [{
                 label: '# of Votes',
                 data: [12, 19, 3, 5, 2, 3],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
@@ -88,6 +88,36 @@ const ctx = document.getElementById('myChart');
                     'rgba(75, 192, 192, 1)',
                     'rgba(153, 102, 255, 1)',
                     'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: false,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+
+
+    const ctx2 = document.getElementById('lineChart');
+    const lineChart = new Chart(ctx2, {
+        type: 'line',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                //255, 193, 7
+                backgroundColor: [
+                    'rgba(56, 72, 80, 0.8)'
+                ],
+                borderColor: [
+                    'rgba(56, 72, 80, 1)',
                 ],
                 borderWidth: 1
             }]
